3c5e6dcea8536688830745dbc77ee7af06ee6115
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-04-14 18:45:04 -0400

    Initial Commit

---
+++ Fruits App/Fruits_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruits_AppApp.swift
+//  Fruits App
+//
+//  Created by Adriancys Jesus Villegas Toro on 14/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Fruits_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
